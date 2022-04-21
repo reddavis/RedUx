f2ff56b8fe16ed36a5713ba903dcfb9c4d17b3e0
Red <me@red.to>
2022-04-21 06:58:42 +0100

    Run task on main actor

--- RedUx/Source/Store.swift
+++ RedUx/Source/Store.swift
@@ -88,15 +88,17 @@ public final class Store<State, Event, Environment> {
             let event = self.eventBacklog.removeFirst()
             let eventStream = self.reducer(&state, event, self.environment)
             
-            Task { [state] in
-                for middleware in self.middlewares {
-                    await middleware.execute(event: event, state: { state })
-                }
-                
-                guard let eventStream = eventStream else { return }
-                for await event in eventStream {
-                    self.send(event)
-                }
+            Task { @MainActor [state] in
+//                await MainActor.run {
+                    for middleware in self.middlewares {
+                        await middleware.execute(event: event, state: { state })
+                    }
+                    
+                    guard let eventStream = eventStream else { return }
+                    for await event in eventStream {
+                        self.send(event)
+                    }
+//                }
             }
         }
     }
